459c68c30466c23f69f3d32c9a2bd0f8668525e9
Eric Taïeb Walch <eric@teknologism.com>
2015-05-26 16:26:29 +0200

    Fix for 4.2.X compatibility

--- exo-activity-filters-service/src/main/java/com/teknologism/exo/addons/customization/listener/CustomRelationshipPublisher.java
+++ exo-activity-filters-service/src/main/java/com/teknologism/exo/addons/customization/listener/CustomRelationshipPublisher.java
@@ -33,158 +33,156 @@ public class CustomRelationshipPublisher extends RelationshipListenerPlugin {
     private static final Log LOG = ExoLogger.getExoLogger(CustomRelationshipPublisher.class);
 
     public static enum TitleId {
-        CONNECTION_REQUESTED,
-        CONNECTION_CONFIRMED
+    CONNECTION_REQUESTED,
+    CONNECTION_CONFIRMED
+  }
+
+  public static final String SENDER_PARAM = "SENDER";
+  public static final String RECEIVER_PARAM = "RECEIVER";
+  public static final String RELATIONSHIP_UUID_PARAM = "RELATIONSHIP_UUID";
+  public static final String RELATIONSHIP_ACTIVITY_TYPE = "exosocial:relationship";
+  public static final String NUMBER_OF_CONNECTIONS = "NUMBER_OF_CONNECTIONS";
+  public static final String USER_ACTIVITIES_FOR_RELATIONSHIP = "USER_ACTIVITIES_FOR_RELATIONSHIP";
+  public static final String USER_COMMENTS_ACTIVITY_FOR_RELATIONSHIP = "USER_COMMENTS_ACTIVITY_FOR_RELATIONSHIP";
+  public static final String USER_DISPLAY_NAME_PARAM = "USER_DISPLAY_NAME_PARAM";
+
+  private ActivityManager  activityManager;
+
+  private IdentityManager identityManager;
+
+  public CustomRelationshipPublisher(InitParams params, ActivityManager activityManager, IdentityManager identityManager) {
+    this.activityManager = activityManager;
+    this.identityManager = identityManager;
+  }
+
+  private ExoSocialActivity createNewActivity(Identity identity, int nbOfConnections) {
+    ExoSocialActivity activity = new ExoSocialActivityImpl();
+    activity.setType(USER_ACTIVITIES_FOR_RELATIONSHIP);
+    updateActivity(activity, nbOfConnections);
+    return activity;
+  }
+
+  private ExoSocialActivity createNewComment(Identity userIdenity, String fullName) {
+    ExoSocialActivityImpl comment = new ExoSocialActivityImpl();
+    comment.setType(USER_COMMENTS_ACTIVITY_FOR_RELATIONSHIP);
+    String message = String.format("I'm now connected with %s", fullName);
+    comment.setTitle(message);
+    comment.setUserId(userIdenity.getId());
+    I18NActivityUtils.addResourceKey(comment, "user_relation_confirmed", fullName);
+    return comment;
+  }
+
+  private void updateActivity(ExoSocialActivity activity, int numberOfConnections) {
+    String title = String.format("I'm now connected with %s user(s)", numberOfConnections);
+    String titleId = "user_relations";
+    Map<String,String> params = new HashMap<String,String>();
+    activity.setTitle(title);
+    activity.setTitleId(null);
+    activity.setTemplateParams(params);
+    I18NActivityUtils.addResourceKey(activity, titleId, "" + numberOfConnections);
+  }
+
+  /**
+   * Publish an activity on both user's stream to indicate their new connection
+   */
+  public void confirmed(RelationshipEvent event) {
+    Relationship relationship = event.getPayload();
+    try {
+      Identity sender = identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, relationship.getSender().getRemoteId(), true);
+                 Identity receiver = identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, relationship.getReceiver().getRemoteId(), true);
+                 LOG.info("CustomRelationshipPublisher: user " + sender.getProfile().getFullName() + " connected with " + receiver.getProfile().getFullName());
+
+    } catch (Exception e) {
+      LOG.warn("Failed to publish event " + event + ": " + e.getMessage());
     }
+  }
 
-    public static final String SENDER_PARAM = "SENDER";
-    public static final String RECEIVER_PARAM = "RECEIVER";
-    public static final String RELATIONSHIP_UUID_PARAM = "RELATIONSHIP_UUID";
-    public static final String RELATIONSHIP_ACTIVITY_TYPE = "exosocial:relationship";
-    public static final String NUMBER_OF_CONNECTIONS = "NUMBER_OF_CONNECTIONS";
-    public static final String USER_ACTIVITIES_FOR_RELATIONSHIP = "USER_ACTIVITIES_FOR_RELATIONSHIP";
-    public static final String USER_COMMENTS_ACTIVITY_FOR_RELATIONSHIP = "USER_COMMENTS_ACTIVITY_FOR_RELATIONSHIP";
-    public static final String USER_DISPLAY_NAME_PARAM = "USER_DISPLAY_NAME_PARAM";
-
-    private ActivityManager activityManager;
-
-    private IdentityManager identityManager;
-
-    public CustomRelationshipPublisher(InitParams params, ActivityManager activityManager, IdentityManager identityManager) {
-
-        this.activityManager = activityManager;
-        this.identityManager = identityManager;
+  private void reloadIfNeeded(Identity identity) throws Exception {
+    if (identity.getId() == null || identity.getProfile().getFullName().length() == 0) {
+      identity = identityManager.getIdentity(identity.getGlobalId().toString(), true);
     }
+  }
+
+  @Override
+  public void ignored(RelationshipEvent event) {
+    ;// void on purpose
+  }
+
+  @Override
+  public void removed(RelationshipEvent event) {
+    Relationship relationship = event.getPayload();
+    try {
+
+      Identity sender = identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, relationship.getSender().getRemoteId(), true);
+      Identity receiver = identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, relationship.getReceiver().getRemoteId(), true);
+      LOG.info("CustomRelationshipPublisher: user " + sender.getProfile().getFullName() + " disconnected from " + receiver.getProfile().getFullName());
 
 
-    private ExoSocialActivity createNewActivity(Identity identity, int nbOfConnections) {
-        ExoSocialActivity activity = new ExoSocialActivityImpl();
-        activity.setType(USER_ACTIVITIES_FOR_RELATIONSHIP);
-        updateActivity(activity, nbOfConnections);
-        return activity;
+    } catch (Exception e) {
+      LOG.debug("Failed to update relationship activity when remove connection : " + e.getMessage());
     }
+  }
 
-    private ExoSocialActivity createNewComment(Identity userIdenity, String fullName) {
-        ExoSocialActivityImpl comment = new ExoSocialActivityImpl();
-        comment.setType(USER_COMMENTS_ACTIVITY_FOR_RELATIONSHIP);
-        String message = String.format("I'm now connected with %s", fullName);
-        comment.setTitle(message);
-        comment.setUserId(userIdenity.getId());
-        I18NActivityUtils.addResourceKey(comment, "user_relation_confirmed", fullName);
-        return comment;
-    }
+  public void denied(RelationshipEvent event) {
+    ;// void on purpose
 
-    private void updateActivity(ExoSocialActivity activity, int numberOfConnections) {
-        String title = String.format("I'm now connected with %s user(s)", numberOfConnections);
-        String titleId = "user_relations";
-        Map<String, String> params = new HashMap<String, String>();
-        activity.setTitle(title);
-        activity.setTitleId(null);
-        activity.setTemplateParams(params);
-        I18NActivityUtils.addResourceKey(activity, titleId, "" + numberOfConnections);
-    }
+  }
 
-    /**
-     * Do not Publish an activity on both user's stream to indicate their new connection but LOG it instead
-     */
-    @Override
-    public void confirmed(RelationshipEvent event) {
-        Relationship relationship = event.getPayload();
-        try {
+  /**
+   * Publish an activity on invited member to show the invitation to connect
+   */
+  public void requested(RelationshipEvent event) {
+//    Relationship relationship = event.getPayload();
+//    try {
+//      Map<String,String> params = this.getParams(relationship);
+//      ExoSocialActivity activity1 = new ExoSocialActivityImpl(relationship.getSender().getId(),
+//                                                              RELATIONSHIP_ACTIVITY_TYPE,
+//                                                              relationship.getSender().getProfile().getFullName() + " has invited "
+//                                                              + relationship.getReceiver().getProfile().getFullName() + " to connect", null);
+//      activity1.setTitleId(TitleId.CONNECTION_REQUESTED.toString());
+//      activity1.setTemplateParams(params);
+//      activityManager.saveActivityNoReturn(relationship.getSender(), activity1);
+//
+//      //TODO hoatle a quick fix for activities gadget to allow deleting this activity
+//      ExoSocialActivity activity2 = new ExoSocialActivityImpl(relationship.getReceiver().getId(),
+//                                                              RELATIONSHIP_ACTIVITY_TYPE,
+//                                                              relationship.getSender().getProfile().getFullName() + " has invited "
+//                                                              + relationship.getReceiver().getProfile().getFullName() + " to connect", null);
+//      activity2.setTitleId(TitleId.CONNECTION_REQUESTED.toString());
+//      activity2.setTemplateParams(params);
+//      activityManager.saveActivityNoReturn(relationship.getReceiver(), activity2);
+//
+//    } catch (Exception e) {
+//      LOG.warn("Failed to publish event " + event + ": " + e.getMessage());
+//    }
+  }
 
-            Identity sender = identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, relationship.getSender().getRemoteId(), true);
-            Identity receiver = identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, relationship.getReceiver().getRemoteId(), true);
-            LOG.info("CustomRelationshipPublisher: user " + sender.getProfile().getFullName() + " connected with " + receiver.getProfile().getFullName());
+  /**
+   * Gets params (sender, receiver, relationship uuid) from a provided relationship.
+   *
+   * @param relationship
+   * @return
+   * @since 1.2.0-GA
+   */
+  private Map<String,String> getParams(Relationship relationship) throws Exception {
+    Identity sender = relationship.getSender();
+    reloadIfNeeded(sender);
+    Identity receiver = relationship.getReceiver();
+    reloadIfNeeded(receiver);
+    String senderRemoteId = sender.getRemoteId();
+    String receiverRemoteId = receiver.getRemoteId();
+    Map<String,String> params = new HashMap<String,String>();
+    params.put(SENDER_PARAM, senderRemoteId);
+    params.put(RECEIVER_PARAM, receiverRemoteId);
+    params.put(RELATIONSHIP_UUID_PARAM, relationship.getId());
+    return params;
+  }
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+  private IdentityStorage getIdentityStorage() {
+    return (IdentityStorage) PortalContainer.getInstance().getComponentInstanceOfType(IdentityStorage.class);
+  }
 
-    }
-
-    @Override
-    public void ignored(RelationshipEvent event) {
-        LOG.info("CustomRelationshipPublisher: ignored");
-    }
-
-    @Override
-    public void removed(RelationshipEvent event) {
-        Relationship relationship = event.getPayload();
-                try {
-
-                    Identity sender = identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, relationship.getSender().getRemoteId(), true);
-                    Identity receiver = identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, relationship.getReceiver().getRemoteId(), true);
-                    LOG.info("CustomRelationshipPublisher: user " + sender.getProfile().getFullName() + " disconnected from " + receiver.getProfile().getFullName());
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-
-    }
-
-    @Override
-    public void requested(RelationshipEvent event) {
-        Relationship relationship = event.getPayload();
-                     try {
-
-                         Identity sender = identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, relationship.getSender().getRemoteId(), true);
-                         Identity receiver = identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, relationship.getReceiver().getRemoteId(), true);
-                         LOG.info("CustomRelationshipPublisher: user " + sender.getProfile().getFullName() + " request connection with " + receiver.getProfile().getFullName());
-
-                     } catch (Exception e) {
-                         e.printStackTrace();
-                     }
-    }
-
-    @Override
-    public void denied(RelationshipEvent event) {
-        Relationship relationship = event.getPayload();
-                     try {
-
-                         Identity sender = identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, relationship.getSender().getRemoteId(), true);
-                         Identity receiver = identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, relationship.getReceiver().getRemoteId(), true);
-                         LOG.info("CustomRelationshipPublisher: user " + sender.getProfile().getFullName() + " denied connection to " + receiver.getProfile().getFullName());
-
-                     } catch (Exception e) {
-                         e.printStackTrace();
-                     }
-    }
-
-
-    private void reloadIfNeeded(Identity identity) throws Exception {
-        if (identity.getId() == null || identity.getProfile().getFullName().length() == 0) {
-            identity = identityManager.getIdentity(identity.getGlobalId().toString(), true);
-        }
-    }
-
-
-    /**
-     * Gets params (sender, receiver, relationship uuid) from a provided relationship.
-     *
-     * @param relationship
-     * @return
-     * @since 1.2.0-GA
-     */
-    private Map<String, String> getParams(Relationship relationship) throws Exception {
-        Identity sender = relationship.getSender();
-        reloadIfNeeded(sender);
-        Identity receiver = relationship.getReceiver();
-        reloadIfNeeded(receiver);
-        String senderRemoteId = sender.getRemoteId();
-        String receiverRemoteId = receiver.getRemoteId();
-        Map<String, String> params = new HashMap<String, String>();
-        params.put(SENDER_PARAM, senderRemoteId);
-        params.put(RECEIVER_PARAM, receiverRemoteId);
-        params.put(RELATIONSHIP_UUID_PARAM, relationship.getId());
-        return params;
-    }
-
-    private IdentityStorage getIdentityStorage() {
-        return (IdentityStorage) PortalContainer.getInstance().getComponentInstanceOfType(IdentityStorage.class);
-    }
-
-    private RelationshipManager getRelationShipManager() {
-        return (RelationshipManager) PortalContainer.getInstance().getComponentInstanceOfType(RelationshipManager.class);
-    }
+  private RelationshipManager getRelationShipManager() {
+    return (RelationshipManager) PortalContainer.getInstance().getComponentInstanceOfType(RelationshipManager.class);
+  }
 }
